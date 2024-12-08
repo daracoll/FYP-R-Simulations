6df80d3f43f2ea9aa6dd7940b10e3f63c9db41cf
daracoll <daracolleran1@gmail.com>
2024-12-08 15:25:54 +0000

    Update Non-Parametric Estimator Simulation Multiple Coefficients.R

--- Non-Parametric Estimator Simulation Multiple Coefficients.R
+++ Non-Parametric Estimator Simulation Multiple Coefficients.R
@@ -1,8 +1,8 @@
 ```{r}
 
-k <- 3 # Choose how many basis functions are associated to X
+p <- 3 # Choose how many basis functions are associated to X
 
-true_coefficients <- runif(k, min = 0.5, max = 5)
+true_coefficients <- runif(p, min = 0.5, max = 5)
 
 sigma_Z <- 1
 sigma_U <- 1
@@ -18,7 +18,7 @@ epsilon_Y <- rnorm(n, mean = 0, sd = sigma_Y)
 Z <- epsilon_Z
 X <- Z^3 + U + epsilon_X
 
-Y <- rowSums(sapply(1:k, function(i) true_coefficients[i] * X^i)) + U + epsilon_Y
+Y <- rowSums(sapply(1:p, function(i) true_coefficients[i] * X^i)) + U + epsilon_Y
 
 data <- data.frame(Z = Z, X = X, Y = Y)
 
@@ -26,11 +26,11 @@ X <- data$X
 Y <- data$Y
 Z <- data$Z
 
-A <- matrix(0, nrow = k, ncol = k)
-b <- numeric(k)
+A <- matrix(0, nrow = p, ncol = p)
+b <- numeric(p)
 
-for (i in 1:k) {
-  for (j in 1:k) {
+for (i in 1:p) {
+  for (j in 1:p) {
     A[i, j] <- sum(Z^i * X^j)
   }
   b[i] <- sum(Z^i * Y)
@@ -43,6 +43,7 @@ print(params)
 
 cat("True coefficients:\n")
 print(true_coefficients)
+
 
 ```
 
@@ -62,18 +63,18 @@ Z <- epsilon_Z
 X <- Z^3 + U + epsilon_X
 Y <- NULL
 
-failed_k <- c()
+failed_p <- c()
 inv_condition_numbers <- c()
 
-for (k in 1:25) {
-  true_coefficients <- runif(k, min = 0.5, max = 5)
-  Y <- rowSums(sapply(1:k, function(i) true_coefficients[i] * X^i)) + U + epsilon_Y
+for (p in 1:25) {
+  true_coefficients <- runif(p, min = 0.5, max = 5)
+  Y <- rowSums(sapply(1:p, function(i) true_coefficients[i] * X^i)) + U + epsilon_Y
   
-  A <- matrix(0, nrow = k, ncol = k)
-  b <- numeric(k)
+  A <- matrix(0, nrow = p, ncol = p)
+  b <- numeric(p)
   
-  for (i in 1:k) {
-    for (j in 1:k) {
+  for (i in 1:p) {
+    for (j in 1:p) {
       A[i, j] <- sum(Z^i * X^j)
     }
     b[i] <- sum(Z^i * Y)
@@ -85,22 +86,23 @@ for (k in 1:25) {
     params <- solve(A, b)
     inv_cond_num
   }, error = function(e) {
-    failed_k <<- c(failed_k, k)
+    failed_p <<- c(failed_p, p)
     inv_cond_num <- 1 / kappa(A)
     inv_condition_numbers <<- c(inv_condition_numbers, inv_cond_num)
     inv_cond_num
   })
 }
 
-if (length(failed_k) > 0) {
+if (length(failed_p) > 0) {
   plot(
-    failed_k, inv_condition_numbers,
+    failed_p, inv_condition_numbers,
     type = "b", pch = 19, col = "red",
-    xlab = "Value of k",
+    xlab = "Value of p",
     ylab = "Inverse Condition Number",
-    main = "Matrix Singularity vs. k"
+    main = "Matrix Singularity vs. p"
   )
 } else {
-  cat("No failures occurred in the range of k.")
+  cat("No failures occurred in the range of p.")
 }
+
 ```
